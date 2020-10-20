15211b85943acd99d662d3d0eebb216ed01ba06f
Fernando Ocampo <fernando.ocampo@avantica.net>
2020-10-19 21:04:07 -0500

    add error buffering approach

--- pkg/domain/employee.go
+++ pkg/domain/employee.go
@@ -7,6 +7,30 @@ import (
 	"github.com/goware/emailx"
 )
 
+// EmployeeValidationError contains employee validation errors
+type EmployeeValidationError struct {
+	Validations []error
+}
+
+func (e *EmployeeValidationError) Error() string {
+	if len(e.Validations) == 1 {
+		return e.Validations[0].Error()
+	}
+	return "employee doesn't contain valid data"
+}
+
+// AddressValidationError contains address validation errors
+type AddressValidationError struct {
+	Validations []error
+}
+
+func (a *AddressValidationError) Error() string {
+	if len(a.Validations) == 1 {
+		return a.Validations[0].Error()
+	}
+	return "address doesn't contain valid data"
+}
+
 // Employee contains important data for employees.
 type Employee struct {
 	UserName string
@@ -54,6 +78,59 @@ func (a Address) ValidateInABasicWay() error {
 	err := postcode.Validate(a.Zip)
 	if err != nil {
 		return errors.New("zip code is not a valid")
+	}
+	return nil
+}
+
+// ValidateInAFullBasicWay validates an employee in a basic way
+// returning all the possible errors.
+func (e Employee) ValidateInAFullBasicWay() error {
+	var result []error
+	if e.UserName == "" {
+		result = append(result, errors.New("username is mandatory"))
+	}
+	emailErr := emailx.Validate(e.Email)
+	if emailErr != nil {
+		result = append(result, errors.New("given Email is not valid"))
+	}
+	addressErr := e.Address.ValidateInAFullBasicWay()
+	if addressErr != nil {
+		if err, ok := addressErr.(*AddressValidationError); ok {
+			result = append(result, err.Validations...)
+		} else {
+			result = append(result, addressErr)
+		}
+	}
+	if len(result) > 0 {
+		return &EmployeeValidationError{
+			Validations: result,
+		}
+	}
+	return nil
+}
+
+// ValidateInAFullBasicWay validates an address in a basic way
+// returning all the possible errors.
+func (a Address) ValidateInAFullBasicWay() error {
+	var result []error
+	if a.Street == "" {
+		result = append(result, errors.New("street is mandatory"))
+	}
+	if a.City == "" {
+		result = append(result, errors.New("city is mandatory"))
+	}
+
+	if a.Zip != "" {
+		err := postcode.Validate(a.Zip)
+		if err != nil {
+			result = append(result, errors.New("zip code is not a valid"))
+		}
+	}
+
+	if len(result) > 0 {
+		return &AddressValidationError{
+			Validations: result,
+		}
 	}
 	return nil
 }
--- pkg/domain/employee_test.go
+++ pkg/domain/employee_test.go
@@ -89,3 +89,94 @@ func TestEmployeeDataBasicValidator(t *testing.T) {
 		assert.Equal(t, v.want, err, testname)
 	}
 }
+
+func TestValidateInAFullBasicWay(t *testing.T) {
+	// GIVEN
+	testcases := map[string]struct {
+		givenEmployee domain.Employee
+		want          []error
+	}{
+		"valid_employee": {
+			givenEmployee: domain.Employee{
+				UserName: "anyname",
+				Email:    "anyname@gmail.com",
+				Address: domain.Address{
+					Street: "quinta",
+					City:   "Cali",
+					Zip:    "760001",
+				},
+			},
+			want: nil,
+		},
+		"empty_username": {
+			givenEmployee: domain.Employee{
+				Email: "anyname@gmail.com",
+				Address: domain.Address{
+					Street: "quinta",
+					City:   "Cali",
+					Zip:    "760001",
+				},
+			},
+			want: []error{fmt.Errorf("username is mandatory")},
+		},
+		"empty_email": {
+			givenEmployee: domain.Employee{
+				UserName: "anyname",
+				Address: domain.Address{
+					Street: "quinta",
+					City:   "Cali",
+					Zip:    "760001",
+				},
+			},
+			want: []error{fmt.Errorf("given Email is not valid")},
+		},
+		"empty_street": {
+			givenEmployee: domain.Employee{
+				UserName: "anyname",
+				Email:    "anyname@gmail.com",
+				Address: domain.Address{
+					City: "Cali",
+					Zip:  "760001",
+				},
+			},
+			want: []error{fmt.Errorf("street is mandatory")},
+		},
+		"empty_city": {
+			givenEmployee: domain.Employee{
+				UserName: "anyname",
+				Email:    "anyname@gmail.com",
+				Address: domain.Address{
+					Street: "quinta",
+					Zip:    "760001",
+				},
+			},
+			want: []error{fmt.Errorf("city is mandatory")},
+		},
+		"invalid_zip": {
+			givenEmployee: domain.Employee{
+				UserName: "anyname",
+				Email:    "anyname@gmail.com",
+				Address: domain.Address{
+					Street: "quinta",
+					City:   "Cali",
+					Zip:    "234d",
+				},
+			},
+			want: []error{fmt.Errorf("zip code is not a valid")},
+		},
+	}
+
+	for testname, v := range testcases {
+		got := v.givenEmployee.ValidateInAFullBasicWay()
+		if v.want == nil && got == nil {
+			continue
+		}
+		err, ok := got.(*domain.EmployeeValidationError)
+		if !ok {
+			t.Errorf("%q: expected EmployeeValidationError but got other: %s", testname, err)
+			t.FailNow()
+		}
+
+		assert.Equal(t, v.want, err.Validations, testname)
+	}
+}
